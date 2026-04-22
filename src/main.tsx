53e75525e602915586d972944d736afa61c6a03b
Skip <skip@usecurling.com>
2026-04-22 18:32:21 +0000

    Auto-commit after sync

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,7 @@
 /* Main entry point for the application - renders the root React component */
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 import App from './App.tsx'
 import './main.css'
 
 // @skip-protected: Do not remove. Required for React rendering.
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+createRoot(document.getElementById('root')!).render(<App />)
